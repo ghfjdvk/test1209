8dda0f0c26eb9d439375ee1c3a0ee469353cebb2
Ryuhs <ghfjdvk@naver.com>
2016-12-09 12:49:22 +0900

    test1209

---
+++ app/src/main/java/com/example/ghfjd_000/test1209_1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.ghfjd_000.test1209_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.R.attr.button;
+import static com.example.ghfjd_000.test1209_1.R.id.button2;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        }
+}
